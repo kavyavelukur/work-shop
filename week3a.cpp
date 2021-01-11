8afaa689f8e79df7b45d8beeb072db1147c2d42d
kavyavelukur <73578255+kavyavelukur@users.noreply.github.com>
2021-01-11 16:08:07 +0530

    Add files via upload

---
+++ week3a.cpp
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main(){
+	int n,i=2,j,x;
+	printf("Enter the n value: ");
+	scanf("%d",&n);
+	if(n>1) printf("The prime numbers from 1 to %d are ",n);
+	while(i<n){
+		j=2; x=0;
+		while(j<i){
+			if(i%j==0){
+				x=1;
+				break;
+			}
+			j++;
+		}
+		if(x==0) printf("%d, ",i);
+		i++;
+	}
+	printf("\n");
+	return 0;
+}
+OUTPUT:
+Enter the n value: 5
+The prime numbers from 1 to 5 are 2, 3,
+
